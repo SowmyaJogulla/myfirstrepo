27c56ba300296803578b9bc3f9e6845fde9d36cf
sowmya <15h61a0578@cvsr.ac.in>
2021-08-29 15:30:12 +0000

    resolved conflicts

--- Hello.java
+++ Hello.java
@@ -2,9 +2,9 @@ class Hello
 {
 public static void main(String args[])
 {
-<<<<<<< HEAD
-System.out.println("hello master");
-=======
+
+
+
 System.out.println("hello feature101");
 >>>>>>> feature101
 }
